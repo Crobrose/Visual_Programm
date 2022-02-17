a21115ab100eaefcd361d57011ad366372fefc00
Crobrose <Crobrose.noreply.github.com>
2022-02-17 19:29:11 +0700

    New Files

---
+++ Lab1 VisualProg/Lab1 VisualProg/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello, World!");
+            Console.Write(n);
+        }
+        catch
+        {
+            Console.WriteLine("Error!!!");
+        }
+        Console.ReadLine();
+    }
+}
+
+
